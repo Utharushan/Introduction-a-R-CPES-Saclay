35ce8aaeb49fb5d3c16332235be8dda9dadb863e
Utharushan <utharushan@gmail.com>
2025-04-10 15:19:16 +0200

    Update brouillon.R

--- brouillon.R
+++ brouillon.R
@@ -1,10 +1,12 @@
+rm(list = ls())
+
 #-------------------------------------------------------------------------------
 # var <- c(1.1, 4.5, 3.23)
 # print(var)
 # names(var) <- c("largeur", "longueur", "hauteur")
 # print(var["longueur"])
 # 
-# vect1 <- c(1,2,34,5,6, NA)
+# vect1 <- c(1, 2, 34, 5, 6, NA)
 # 
 # note <- 10
 # if (note >= 10.0) {
@@ -40,19 +42,16 @@ Table
 
 #-------------------------------------------------------------------------------
 
-pays <- c("Afrique du Sud","Allemagne","Arabie Saoudite","Argentine","Australie","Brésil","Canada","Chine","Corée du Sud","Etats-Unis","France","Inde","Indonésie","Italie","Japon","Mexique","Royaume-Uni","Russie","Turquie")
-pib <-c(380,4526,1068,646,1728,2174,2142,17790,1713,27720,3052,3568,1371,2301,4204,1789,3381,2021,1118)
-nbhab <- c(63,83,33,46,27,211,40,1411,52,340,68,1438,281,59,125,130,68,144,85)
+pays <- c("Afrique du Sud", "Allemagne", "Arabie Saoudite", "Argentine", "Australie", "Brésil", "Canada", "Chine", "Corée du Sud", "Etats-Unis", "France", "Inde", "Indonésie", "Italie", "Japon", "Mexique", "Royaume-Uni", "Russie", "Turquie")
+pib <- c(380, 4526, 1068, 646, 1728, 2174, 2142, 17790, 1713, 27720, 3052, 3568, 1371, 2301, 4204, 1789, 3381, 2021, 1118)
+nbhab <- c(63, 83, 33, 46, 27, 211, 40, 1411, 52, 340, 68, 1438, 281, 59, 125, 130, 68, 144, 85)
 Table <- array(data = c(pib, nbhab), dim = c(19, 2))
 rownames(Table) <- pays
 colnames(Table) <- c("Pib", "Nb Habitants")
-pibhab <- pib/nbhab
+pibhab <- pib / nbhab
 Table = cbind(Table, pibhab)
 colnames(Table) <- c("PIB", "NbHab", "PibHab")
 pibhab
-Ord_Table = Table[order(Table[, "PibHab"]),]
+Ord_Table = Table[order(Table[, "PibHab"]), ]
 Ord_Table
 plot(Ord_Table)
-
-#-------------------------------------------------------------------------------
-rm(list = ls())

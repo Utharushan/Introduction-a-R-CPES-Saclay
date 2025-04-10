7e7ffd379710002ef007b53ed2b316a67b5244d4
Utharushan <utharushan@gmail.com>
2025-04-10 15:11:40 +0200

    Add files via upload

---
+++ population/population.R
@@ -0,0 +1,65 @@
+rm (list = ls())
+
+# # Scénario 1
+# nbCompartiments <- 5
+# nbAnnees <- 75
+# effectif_compartiment <- c(50, 0, 0, 0, 0)
+# taux_de_fecondite <- 3.75
+# taux_de_transition <- c(0.75, 0.6, 0.6, 0.5, 0.4937)
+# 
+# # Scénario 2
+# nbCompartiments <- 5
+# nbAnnees <- 30
+# effectif_compartiment <- c(50, 0, 0, 0, 0)
+# taux_de_fecondite <- 5
+# taux_de_transition <- c(0.75, 0.6, 0.6, 0.5, 0.4937)
+#
+# # Scénario 3
+# nbCompartiments <- 5
+# nbAnnees <- 100
+# effectif_compartiment <- c(50, 10, 10, 10, 10)
+# taux_de_fecondite <- 3.25
+# taux_de_transition <- c(0.75, 0.6, 0.6, 0.5, 0.4937)
+# 
+# Scénario 4
+nbCompartiments <- 6
+nbAnnees <- 200
+effectif_compartiment <- c(0, 0, 0, 0, 0, 100)
+taux_de_fecondite <- 4.65
+taux_de_transition <- c(0.72, 0.72, 0.72, 0.72, 0.72, 0.1)
+
+evolution <- c(effectif_compartiment)
+
+for (i in 1:nbAnnees) {
+  temp_compartiment <- effectif_compartiment
+  for (j in 1:nbCompartiments) {
+    if (j == 1) {
+      temp_compartiment[j] <- effectif_compartiment[nbCompartiments] * taux_de_fecondite
+    } else if (j == nbCompartiments) {
+      temp_compartiment[j] <- effectif_compartiment[j] * taux_de_transition[j] + effectif_compartiment[j-1] * taux_de_transition[j-1]
+    } else {
+      temp_compartiment[j] <- effectif_compartiment[j-1] * taux_de_transition[j-1]
+    }
+  }
+  effectif_compartiment <- temp_compartiment
+  evolution <- rbind(evolution, effectif_compartiment)
+}
+
+print(effectif_compartiment)
+print(evolution)
+
+population <- c()
+for (i in 1:nbAnnees) {
+  population <- append(population, sum(evolution[i,]))
+}
+print(population)
+
+png("scenario4.png", width = 800, height = 600)
+
+plot(population, type = "l", col = "blue", lwd = 2, 
+     xlab = "Time (Years)", ylab = "Population", 
+     main = "Population Dynamics Over Time")
+
+grid()
+
+dev.off()
---
+++ proie_predateur/proie_predateur.R
@@ -0,0 +1,31 @@
+rm(list = ls())
+
+survie_proie = 1.05
+survie_predateur = 0.97
+taux_de_predation = 0.001
+croissance_predateur = 0.0002
+nombre_de_cycle = 6000
+
+evolution_proie = rep(0, 6000)
+evolution_predateur = rep(0, 6000)
+
+evolution_proie[1] = 200
+evolution_predateur[1] = 50
+
+for (i in 2:6000) {
+  evolution_proie[i] = evolution_proie[i - 1] * survie_proie -
+    taux_de_predation * evolution_proie[i - 1] * evolution_predateur[i - 1]
+  evolution_predateur[i] = evolution_predateur[i - 1] * survie_predateur +
+    croissance_predateur * evolution_proie[i - 1] * evolution_predateur[i - 1]
+}
+
+evolution_predateur
+evolution_proie
+
+png("proie_predateur.png", width = 800, height = 600)
+
+plot(evolution_proie, type = "l", col = "blue", xlab = "Années", ylab = "Effectif", main = "Proie et prédateur")
+lines(evolution_predateur, col = "red")
+legend("topright", legend = c("Proie", "Prédateur"), col = c("blue", "red"), lty = 1)
+
+dev.off()
